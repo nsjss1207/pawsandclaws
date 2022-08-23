6738ba2dd2437b84b80756d322e95ea8bb952bd0
nsjss1207 <107710230+nsjss1207@users.noreply.github.com>
2022-08-23 15:07:02 +0530

    Add files via upload

---
+++ Paws-and-Claws/script2.js
@@ -0,0 +1,9 @@
+const instaBtn = document.querySelector('.insta-img');
+instaBtn.addEventListener('click', function () {
+  window.open('https://www.instagram.com/pawsandclawslko/?hl=en');
+});
+
+const facebookBtn = document.querySelector('.facebook-img');
+facebookBtn.addEventListener('click', function () {
+  window.open('https://www.facebook.com/pawsanddclaws/');
+});
